ee64c6a1c4df5b3f86c7aa5bd5655b0de1421651
Lillisip <Lillisip@yandex.ru>
2025-06-20 19:18:04 +0300

    Lombok

---
+++ src/main/java/ru/yandex/practicum/catsgram/model/User.java
@@ -0,0 +1,15 @@
+package ru.yandex.practicum.catsgram.model;
+
+import lombok.Data;
+
+import java.time.Instant;
+
+@Data
+public class User {
+    Long id;
+    String username;
+    String email;
+    String password;
+    Instant registrationDate;
+
+}
